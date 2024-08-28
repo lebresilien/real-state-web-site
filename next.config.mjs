4e948a9ef58db4145aae4ab78d7a401b0e72eeef
lebresilien <petitotapondjou@gmail.com>
2024-08-28 17:54:56 +0100

    image source

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
           },
           {
             protocol: 'https',
-            hostname: 'etsmanagementimmo.com'
+            hostname: 'backend.etsmanagementimmo.com'
           },
         ],
     },
