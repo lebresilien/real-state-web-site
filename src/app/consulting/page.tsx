05bcd564a34db411f45c9f254a27f17bdaf00159
lebresilien <petitotapondjou@gmail.com>
2024-10-01 11:00:01 +0100

    swith contact to consulting

--- src/app/consulting/page.tsx
+++ src/app/consulting/page.tsx
@@ -395,7 +395,8 @@ export default function Consulting() {
                                                                 selected={field.value}
                                                                 onSelect={field.onChange}
                                                                 disabled={(date) =>
-                                                                date > new Date() || date < new Date("1900-01-01")
+                                                                    // date > new Date() || date < new Date("1900-01-01")
+                                                                    date < new Date()
                                                                 }
                                                                 initialFocus
                                                             />
